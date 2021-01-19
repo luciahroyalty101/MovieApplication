19811a82db9f6c31a0b799d7cc8b725fdb0c9cbb
luciahroyalty101 <luciahkei101@gmail.com>
2021-01-19 03:23:01 +0300

    added internet peermissions in manifests

--- app/src/main/java/com/moringaschool/wallview1/MainActivity.java
+++ app/src/main/java/com/moringaschool/wallview1/MainActivity.java
@@ -1,8 +1,11 @@
 package com.moringaschool.wallview1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TabLayout tabLayout = findViewById(R.id.tablayout);
+        ViewPager viewPager = findViewById(R.id.viewpager);
+
+        TabPageAdapter tabPageAdapter = new TabPageAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(tabPageAdapter);
+
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
